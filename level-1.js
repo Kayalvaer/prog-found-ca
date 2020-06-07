d7531c01a6c7cd524ab870b8482dcc5b328eb557
Kayalvaer <62592167+Kayalvaer@users.noreply.github.com>
2020-06-07 23:58:13 +0200

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,99 @@
+//Section 1
+// question 1
+
+// Answer for question 1 
+
+var fruitName = "apple";
+
+// question 2
+
+// Answer for question 2 
+var person = {
+    name: "Leon",
+    age = 3
+};
+
+// question 3
+
+// Answer for question 3
+var outOfStock = true;
+
+if (outOfStock === true) {
+    console.log("outOfStock");
+}else{
+    console.log("in stock");
+}
+
+// question 4
+
+// Answer for question 4
+var numberArray[1, 2, 3, 4, 5];
+    console.log(numberArray);
+
+for (var i = 0; i < numberArray.length; i++) {
+    console.log (numberArray[i]);
+}
+
+
+// question 5
+
+// Answer for question 5
+var counter;
+for (var i = 15; i <= 25; i++) {
+    console.log[i];
+}
+
+
+// question 6
+
+// Answer for question 6
+var counter;
+for (var i = 15; i <= 25; i++) {
+    console.log[i];
+}
+if (i === 20){
+    console.log(i)
+}else {
+    console.log(counter[i]);
+}
+
+
+
+// question 7
+
+// Answer for question 7
+var countries = [
+    {name: "Botswana", population: 2.2, isLargerSize:false}
+    {name: "Norway", population: 5.3, isLargeSize:true}
+];
+
+for (var i = 0; i < countries.length; i++) {
+    console.log(countries[i].population);
+    console.log(countries[i].isLargeSize);
+}
+
+// question 8
+
+// Answer for question 8
+function whatIDontLike(realLies) {
+    console.log ("i don´t like" + realLies);
+}
+whatIDontLike("lies")
+
+// question 9
+
+// Answer for question 9
+function contResult(cont1, cont2) {
+    console.log(cont2 - cont1);
+}
+contResult(50, 130);
+
+
+// question 10
+
+// Answer for question 10
+var foodItem = [ ];
+var foodItem(theFood) {
+    console.log(theFood);
+}
+logToConsole("pasta");
